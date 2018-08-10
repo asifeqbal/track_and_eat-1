dbd063261d1e7022e4282458b28baabd5050cfa3
Asif Eqbal <asif.eqbal@vrisini.com>
2018-08-10 14:25:58 +0530

    add qr plugin

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -8,12 +8,21 @@ module.exports = [
       "plugin.printer",
       "cordova.plugins.printer"
     ]
+  },
+  {
+    "id": "cordova-plugin-qrscanner.QRScanner",
+    "file": "plugins/cordova-plugin-qrscanner/www/www.min.js",
+    "pluginId": "cordova-plugin-qrscanner",
+    "clobbers": [
+      "QRScanner"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-printer": "0.7.3"
+  "cordova-plugin-printer": "0.7.3",
+  "cordova-plugin-qrscanner": "2.6.0"
 };
 // BOTTOM OF METADATA
 });
